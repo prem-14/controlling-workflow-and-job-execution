03706202fb9e26ea8af6c06c6a625d615af95f28
Premkumar1499 <premkumar.ar99github@gmail.coml>
2023-05-01 11:08:06 +0530

    Added text in helpbox

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -1,19 +1,20 @@
-import { string } from 'prop-types';
+import { string } from 'prop-types'
 
-import './HelpBox.css';
+import './HelpBox.css'
 
 function HelpBox({ title, text }) {
   return (
-    <article className="help-box">
+    <article className='help-box'>
       <h2>{title}</h2>
       <p>{text}</p>
+      <h3>help</h3>
     </article>
-  );
+  )
 }
 
 HelpBox.propTypes = {
   title: string,
   text: string,
-};
+}
 
-export default HelpBox;
+export default HelpBox
